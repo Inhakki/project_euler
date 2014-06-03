2cedc301e4dd77bcf8d21d1f3ab6d5b705cf1607
Inhak Kim <inhakki@gmail.com>
2014-06-03 10:13:44 -0400

    made more efficient

--- problem_2.js
+++ problem_2.js
@@ -8,12 +8,8 @@ while (lastNum < 4000000) {
   fibseq.push(lastNum + secondLastNum);
   lastNum = fibseq[fibseq.length-1];
   secondLastNum = fibseq[fibseq.length-2];
-}
-
-for (i = 0; i < fibseq.length; i++){
-  if (fibseq[i] % 2 === 0){
-    total += fibseq[i];
+  if (lastNum % 2 === 0){
+    total += lastNum
   }
 }
-
 console.log(total);
